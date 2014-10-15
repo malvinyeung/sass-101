f45f62d96279a79b26c24a9a3d0a2310f50c9ced
Malvin Yeung <malvinyeung@gmail.com>
2014-10-15 11:31:33 -0700

    updates

--- Malvin-Craig/malvin-js/malvin.js
+++ Malvin-Craig/malvin-js/malvin.js
@@ -1,8 +1,30 @@
-var form = new Object();
-var output = document.getElementById("output");
+function Form(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+}
 
-document.getElementById("submit").onClick = function() {
-  form.firstName = document.getElementById("firstName");
-  form.lastName = document.getElementById("lastName");
-  output.textContent = "Thanks for signing up, " + form.firstName + " " + form.lastName + "!";
+var button = document.getElementById("submit");
+
+button.onclick = function() {
+  var output = document.getElementById("output");
+  var firstName = document.getElementById("firstName").value;
+  var lastName = document.getElementById("lastName").value;
+  var form1 = new Form(firstName, lastName);
+  alert("Thanks for signing up, " + form1.firstName + " " + form1.lastName + "!");
+};
+
+
+function Form(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+}
+
+var button = document.getElementById("submit2");
+
+button.onclick = function() {
+  var output = document.getElementById("output");
+  var firstName = document.getElementById("firstName2").value;
+  var lastName = document.getElementById("lastName2").value;
+  var form1 = new Form(firstName, lastName);
+  output.textContent = "Thanks for signing up, " + form1.firstName + " " + form1.lastName + "!";
 };
